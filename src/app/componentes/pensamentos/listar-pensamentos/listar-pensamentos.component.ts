558938bcdac00eaa5564edf45c89bfc1431deb96
Jonatha N. Lima <jnogueira@magnasistemas.com.br>
2023-10-24 17:24:54 -0300

    Adicionando botão de paginação.

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -11,6 +11,7 @@ export class ListarPensamentosComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) { }
 
@@ -20,4 +21,13 @@ export class ListarPensamentosComponent implements OnInit {
     })
   }
 
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos.push(...listaPensamentos);
+        if(this.listaPensamentos.length) {
+          this.haMaisPensamentos = false
+        }
+      })
+  }
 }
